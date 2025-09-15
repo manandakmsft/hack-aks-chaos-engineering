c2780dd1d2296f2b93ab0030be9c99e08bdd65de
manandakmsft <84652228+manandakmsft@users.noreply.github.com>
2025-09-14 21:47:06 -0500

    Update solution.sh

--- Solution/Challenge 05/solution.sh
+++ Solution/Challenge 05/solution.sh
@@ -1,76 +1,41 @@
 CLUSTER_NAME="<cluster-name>"
 RESOURCE_GROUP="<rg>"
-AMW_NAME="<workspace-name>"
-GRAFANA_NAME="<grafana-name"
-LOCATION="<location>">
+AZURE_MONITOR_NAME="<azure-monitor-name>"
+GRAFANA_NAME="<grafana-name>"
+LOCATION="<location>"
 
-# Get your Entra ID (AAD) user object ID
-# This may fail if ran in the portal
-#Run locally and copy result into bash prompt in the portal
-#$(az ad signed-in-user show --query id -o tsv)
-#fill in result of above command for ME_OBJECT_ID
-ME_OBJECT_ID=""
+# Create Azure Monitor resource
+az resource create \
+    --resource-group $RESOURCE_GROUP \
+    --namespace microsoft.monitor \
+    --resource-type accounts \
+    --name $AZURE_MONITOR_NAME \
+    --location eastus \
+    --properties '{}'
 
-
-##### For the most reliable experience, run the below commands one at a time
-#querying for a resource id right after object creation sometime results in empty string
-# and assigning roles can take some time to propagate
-
-#create Azure monitor account
-az monitor account create \
-  --resource-group "$RESOURCE_GROUP" \
-  --name "$AMW_NAME" \
-  --location "$LOCATION"
-
-#store the AMW id for Prometheus destination 
-AMW_ID="$(az monitor account show \
-  --resource-group "$RESOURCE_GROUP" \
-  --name "$AMW_NAME" \
-  --query id -o tsv)"
-
-
-#install grafana and get ID for read permissions for AMW
-#may need to install extension
+# Create Grafana instance
 az grafana create \
-  --resource-group "$RESOURCE_GROUP" \
-  --name "$GRAFANA_NAME" \
-  --sku-tier Standard
+    --name $GRAFANA_NAME \
+    --resource-group $RESOURCE_GROUP 
 
-GRAFANA_ID="$(az grafana show \
-  --resource-group "$RESOURCE_GROUP" \
-  --name "$GRAFANA_NAME" \
-  --query id -o tsv)"
 
-GRAFANA_MI="$(az grafana show \
-  --resource-group "$RESOURCE_GROUP" \
-  --name "$GRAFANA_NAME" \
-  --query identity.principalId -o tsv)"
+# Place the Azure Managed Grafana and Azure Monitor resource IDs in variables
+grafanaId=$(az grafana show \
+                --name $GRAFANA_NAME \
+                --resource-group $RESOURCE_GROUP \
+                --query id \
+                --output tsv)
+azuremonitorId=$(az resource show \
+                    --resource-group $RESOURCE_GROUP \
+                    --name $AZURE_MONITOR_NAME \
+                    --resource-type "Microsoft.Monitor/accounts" \
+                    --query id \
+                    --output tsv)
 
-echo 'Granting reader permissions to Azure monitor workspace...'
-  az role assignment create \
-  --assignee-object-id "$GRAFANA_MI" \
-  --assignee-principal-type ServicePrincipal \
-  --role "Monitoring Reader" \
-  --scope "$AMW_ID"
-echo 'Monitoring Reader added to workspace.' 
-  
-echo 'Adding Grafana viewer role to current user...'
-# Grant Grafana Viewer on the Grafana resource
-az role assignment create \
-  --assignee-object-id "$ME_OBJECT_ID" \
-  --assignee-principal-type User \
-  --role "Grafana Viewer" \
-  --scope "$GRAFANA_ID"
-  
-# #add AMW monitoring contributor 
-echo 'Grant Monitoring contributor to current user on Azure monitor workspace...'
-az role assignment create \
-  --assignee-object-id "$ME_OBJECT_ID" \
-  --assignee-principal-type User \
-  --role "Monitoring Contributor" \
-  --scope "$AMW_ID"
-
-#link AMW to aks cluster 
-echo 'Enabling Azure monitor metrics, configuring Grafana and monitor workspace...'
-az aks update --enable-azure-monitor-metrics --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP --azure-monitor-workspace-resource-id $AMW_ID --grafana-resource-id  $GRAFANA_ID
-
+# Link Azure Monitor and Azure Managed Grafana to the AKS cluster
+az aks update \
+    --name $CLUSTER_NAME \
+    --resource-group $RESOURCE_GROUP \
+    --enable-azure-monitor-metrics \
+    --azure-monitor-workspace-resource-id $azuremonitorId \
+    --grafana-resource-id $grafanaId
